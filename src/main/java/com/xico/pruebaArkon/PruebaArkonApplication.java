231268b350a85176fa8e2c3b379250efa1159159
xico-nogueda <xnogueda95@gmail.com>
2024-10-08 17:52:15 -0600

    Commit Initial

---
+++ src/main/java/com/xico/pruebaArkon/PruebaArkonApplication.java
@@ -0,0 +1,13 @@
+package com.xico.pruebaArkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaArkonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaArkonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xico/pruebaArkon/PruebaArkonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xico.pruebaArkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaArkonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
